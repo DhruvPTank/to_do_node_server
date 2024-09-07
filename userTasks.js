4e282906b9c3c82c0697ccfeee71cd0f6714152f
Neelkanth <dhruvptank2910@gmail.com>
2024-09-07 22:20:06 +0530

    updated code for adding tasks by particular users

--- userTasks.js
+++ userTasks.js
@@ -13,6 +13,27 @@ router.get('/getUserTask', async (req, res) => {
     res.json(userTasks);
 });
 
+router.post('/addUserTask/:userId', async (req, res) => {
+
+    const { userId } = req.params;
+    const { taskName, taskDescription } = req.body;
+    if (!taskName || !taskDescription) {
+        return res.status(400).json({ error: 'Task name and description required' });
+    }
+    const user = await dataBaseUser.findOne({ where: { userID: userId } });
+    if (!user) {
+        return res.status(400).json({ error: 'User not exist in database' });
+    }
+    const newUserTask = await dataBaseUserTask.create({ UserId: userId, taskName, taskDescription });
+    res.send(newUserTask);
+});
+
+
+router.delete('/deleteUesrTask/:userId/:id', async (req, res) => {
+    const { userId, id } = req.params;
+
+
+})
 
 
 module.exports = router;
